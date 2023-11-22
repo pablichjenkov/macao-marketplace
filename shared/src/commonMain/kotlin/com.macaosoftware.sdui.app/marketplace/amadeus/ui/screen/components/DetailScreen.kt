7367ddcb8949b417aa0932160a122f9b05152103
Khubaib Khan <18.bscs.803@gmail.com>
2023-11-22 16:10:53 +0500

    Reusable UI Components & Home Screen Designs (Macao #4)

---
+++ shared/src/commonMain/kotlin/com.macaosoftware.sdui.app/marketplace/amadeus/ui/screen/components/DetailScreen.kt
@@ -0,0 +1,15 @@
+package com.macaosoftware.sdui.app.marketplace.amadeus.ui.screen.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.macaosoftware.component.core.Component
+
+class DetailScreen() : Component() {
+
+    @Composable
+    override fun Content(modifier: Modifier) {
+        Text("Detail Screen")
+    }
+
+}
