dac657d4ed60535e967ecac935d6255200463fdb
pablichjenkov <am.pablo.vc@gmail.com>
2023-11-23 02:47:46 -0500

    - Clean some files (#32)

--- shared/src/commonMain/kotlin/com.macaosoftware.sdui.app/marketplace/amadeus/util/Util.kt
+++ shared/src/commonMain/kotlin/com.macaosoftware.sdui.app/marketplace/amadeus/util/Util.kt
@@ -3,6 +3,6 @@ package com.macaosoftware.sdui.app.marketplace.amadeus.util
 object Util {
     const val BASE_URL = "https://test.api.amadeus.com/v1"
     const val Authorization = "Authorization"
-    const val Token = "Bearer VCs2xXyvpv4rTydIR6TI2mARAnGe"
+    const val Token = ""
     const val IMAGE = "https://23c133e0c1637be1e07d-be55c16c6d91e6ac40d594f7e280b390.ssl.cf1.rackcdn.com/u/gpch/Park-Hotel-Group---Explore---Grand-Park-City-Hall-Facade.jpg"
 }
