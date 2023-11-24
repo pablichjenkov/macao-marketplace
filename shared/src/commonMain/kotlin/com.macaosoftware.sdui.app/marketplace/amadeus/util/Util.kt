56beee0f6c5044f8edee674f1aefb33fddbcb7a0
Khubaib Khan <18.bscs.803@gmail.com>
2023-11-24 13:38:24 +0500

    Merged (#7)

--- shared/src/commonMain/kotlin/com.macaosoftware.sdui.app/marketplace/amadeus/util/Util.kt
+++ shared/src/commonMain/kotlin/com.macaosoftware.sdui.app/marketplace/amadeus/util/Util.kt
@@ -3,7 +3,7 @@ package com.macaosoftware.sdui.app.marketplace.amadeus.util
 object Util {
     const val BASE_URL = "https://test.api.amadeus.com/v1"
     const val Authorization = "Authorization"
-    const val Token = "Bearer HNhCu3Mx1DaJ1gPEsY2NqxnTVMSg"
+    const val Token = "Bearer yeIf4cGZZgb2q4HHU11n9zYF83PU"
     const val IMAGE = "https://23c133e0c1637be1e07d-be55c16c6d91e6ac40d594f7e280b390.ssl.cf1.rackcdn.com/u/gpch/Park-Hotel-Group---Explore---Grand-Park-City-Hall-Facade.jpg"
     const val PROFILE = "https://cdn.vectorstock.com/i/preview-1x/20/76/man-avatar-profile-vector-21372076.jpg"
 }
