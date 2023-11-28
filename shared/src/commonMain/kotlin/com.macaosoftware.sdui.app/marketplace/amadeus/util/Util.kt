01b42a49fbcc0ac539ee853140bc9992654e3c97
Khubaib Khan <18.bscs.803@gmail.com>
2023-11-28 08:45:08 +0500

    Cleanup #43)

--- shared/src/commonMain/kotlin/com.macaosoftware.sdui.app/marketplace/amadeus/util/Util.kt
+++ shared/src/commonMain/kotlin/com.macaosoftware.sdui.app/marketplace/amadeus/util/Util.kt
@@ -2,7 +2,7 @@ package com.macaosoftware.sdui.app.marketplace.amadeus.util
 
 object Util {
     const val Authorization = "Authorization"
-    const val Token = "Bareer d6QAcNPJx8RwD62fz70b643MAgOA"
+    const val Token = ""
     const val IMAGE = "https://23c133e0c1637be1e07d-be55c16c6d91e6ac40d594f7e280b390.ssl.cf1.rackcdn.com/u/gpch/Park-Hotel-Group---Explore---Grand-Park-City-Hall-Facade.jpg"
     const val PROFILE = "https://cdn.vectorstock.com/i/preview-1x/20/76/man-avatar-profile-vector-21372076.jpg"
 }
