460d1a5a098206e9af9adf01c37943ef2ca056eb
Khubaib Khan <18.bscs.803@gmail.com>
2023-12-11 17:19:44 +0500

    -Stripe Integration (#52)

--- androidApp/src/androidMain/kotlin/com/macaosoftware/sdui/app/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/macaosoftware/sdui/app/MainActivity.kt
@@ -11,8 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -20,12 +18,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.macaosoftware.data.repository.Repository
 import com.macaosoftware.plugin.MacaoApplicationState
 import com.macaosoftware.utils.Constant.PUBLISHER_KEY
@@ -231,7 +228,7 @@ fun customColors(): PaymentSheet.Colors {
 }
 fun customPrimaryButtonColors(): PaymentSheet.PrimaryButtonColors {
     return PaymentSheet.PrimaryButtonColors(
-        background  = Color(0xFF00FF00), // Customize enabled state color
+        background = Color(0xFF00FF00), // Customize enabled state color
         onBackground = Color(0xFF808080), // Customize disabled state color
         border = Color(0xFF008000), // Customize pressed state color
     )
