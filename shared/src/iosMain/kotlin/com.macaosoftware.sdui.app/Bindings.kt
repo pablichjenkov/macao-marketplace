f4dda0fdadc0e0f53b50fc0c7139e38c21d04ed1
Pablo Valdes <am.pablo.vc@gmail.com>
2023-12-22 19:13:03 -0500

    - Cleanup

--- shared/src/iosMain/kotlin/com.macaosoftware.sdui.app/Bindings.kt
+++ shared/src/iosMain/kotlin/com.macaosoftware.sdui.app/Bindings.kt
@@ -1,16 +1,12 @@
 package com.macaosoftware.sdui.app
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.ComposeUIViewController
 import com.macaosoftware.app.IosMacaoApplication
 import com.macaosoftware.app.MacaoApplicationState
 import com.macaosoftware.plugin.DefaultPlatformLifecyclePlugin
 import com.macaosoftware.plugin.FirebaseAuth
 import com.macaosoftware.plugin.IosBridge
+import com.macaosoftware.sdui.app.view.SplashScreen
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import platform.UIKit.UIViewController
@@ -27,15 +23,9 @@ fun buildDemoMacaoApplication(
         macaoApplicationState = MacaoApplicationState(
             Dispatchers.IO,
             IosRootComponentProvider(iosBridge, firebaseAuth)
-        )
-    ) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Text(
-                modifier = Modifier.align(Alignment.Center),
-                text = "Example of iOS Splash Screen"
-            )
-        }
-    }
+        ),
+        splashScreenContent = { SplashScreen() }
+    )
 }
 
 // Todo: Replace with swift implementation
