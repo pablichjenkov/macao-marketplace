6c0cbe2cc6ca6843358b2cfa977c82641f4fdd50
Pablo Valdes <am.pablo.vc@gmail.com>
2024-09-14 22:21:14 -0400

    Clean IosBridge remnants Add docs to SwiftWrappersFactory class

--- composeApp/src/iosMain/kotlin/com.macaosoftware.sdui.app/IosBridge.kt
+++
@@ -1,9 +0,0 @@
-package com.macaosoftware.sdui.app
-
-import com.macaosoftware.plugin.account.AccountPlugin
-import com.macaosoftware.plugin.account.AccountPluginEmpty
-
-@ObjCName(name = "IosBridge", exact = true)
-class IosBridge(
-    val accountPlugin: AccountPlugin = AccountPluginEmpty()
-)
