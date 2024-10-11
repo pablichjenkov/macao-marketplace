85a38b50cf59d8ffe46c375db15aa6b5430c7a30
Pablo Valdes <am.pablo.vc@gmail.com>
2024-10-11 18:42:17 -0400

    Introduce Logger Pluggin

---
+++ logger-dev/build.gradle.kts
@@ -0,0 +1,68 @@
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.compose)
+    alias(libs.plugins.android.library)
+}
+
+kotlin {
+    androidTarget()
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "Logger"
+            isStatic = true
+        }
+    }
+
+    js(IR) {
+        browser()
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        moduleName = "Logger"
+        browser()
+        binaries.library()
+    }
+
+    jvm()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.ui)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+        }
+        commonTest.dependencies {
+        }
+        androidMain.dependencies {
+        }
+        jvmMain.dependencies {
+        }
+        jsMain.dependencies {
+        }
+    }
+}
+
+android {
+    namespace = "com.macaosoftware.plugin.logger"
+    compileSdk = libs.versions.androidCompileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.androidMinSdk.get().toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    composeCompiler {
+        enableStrongSkippingMode = true
+    }
+}
